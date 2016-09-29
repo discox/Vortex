8604fcf9bb67e82271c2be8a0175e52042bd12d6
discox <mathieu@peyron.ch>
2016-09-29 14:55:57 +0800

    Create scanx.py

---
+++ scanx.py
@@ -0,0 +1,16 @@
+########### listen in on twitter ###########
+
+#user id: matpeyron pwd: globalreach
+#use nltk (already in Anaconda)? or tweetstream? or tweepy?
+
+print('the program starts')
+
+########### store the data ###########
+
+########### quantify results ###########
+
+########### extract stock market data ###########
+
+########### fit the model to the data ###########
+
+########### test against prediction ###########
