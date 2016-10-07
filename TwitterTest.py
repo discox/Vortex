ada372de71246f73ae95e9906ced253888f71b27
discox <mathieu@peyron.ch>
2016-10-07 06:23:48 +0000

    changes to the twitter API

--- TwitterTest.py
+++ TwitterTest.py
@@ -1,12 +1,39 @@
-#import nltk
+#import twython
+#from nltk.twitter import Twitter
+from nltk.twitter import Query, Streamer, Twitter, TweetViewer, TweetWriter, credsfromfile
 
-from nltk.twitter import Twitter
 
 # need to set the environment variable: export TWITTER="/home/cabox/workspace/twitter-files/credentials.txt"
 
 tw = Twitter()
 
 
-tw.tweets(keywords='Donald, Trump', limit=10)
+#tw.tweets(keywords='Donald, Trump', limit=10) #looks for keywords
 
-#tw.tweets(follow=['759251', '612473'], to_screen=True, limit=10)
+#tw.tweets(follow=['759251', '612473'], to_screen=True, limit=10) #looks for CNN and BBC
+
+#a = tw.tweets(keywords='Clinton, Trump',follow=['759251', '612473'], limit=10)
+
+oauth = credsfromfile()
+client = Query(**oauth)
+
+tweets = client.search_tweets(keywords='qnst', limit=1)
+#tweet = next(tweets)
+
+tweet = next(tweets)
+from pprint import pprint
+pprint(tweet, depth=1)
+#print tweets
+
+#for tweet in tweets:
+#    print tweet['text']
+#    print tweet['created_at']
+#    print tweet['user']
+
+
+
+
+
+
+
+
