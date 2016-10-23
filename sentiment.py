fb819ca8e16cc1cc8a6c400e54c785c5dea891e7
discox <mathieu@peyron.ch>
2016-10-23 09:02:11 +0000

    scanx wireframe setup

---
+++ sentiment.py
@@ -0,0 +1,15 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sentences = ["very bad opinion", "not really sure", "awesome example"]
+
+sid = SentimentIntensityAnalyzer()
+
+for sentence in sentences:
+    print sentence
+    ss = sid.polarity_scores(sentence)
+
+    for k in sorted(ss):
+        print '{0}: {1}, '.format(k, ss[k]) #, end=''
+
+    print
+
